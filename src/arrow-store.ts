d740858689c246787aa8daa084f904f48854e142
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-03-25 08:52:47 -0400

    Update

--- src/arrow-store.ts
+++ src/arrow-store.ts
@@ -2,3 +2,5 @@
 //
 // The constructor should accept an array/buffer of arrow IPC bytes.
 // The zarr store should then expose the table via the same anndata dataframe zarr-based interface as the ParquetAsAnnDataFrameStore.
+
+// To test this, use the tableToIPC and tableFromIPC functions from Flechette in unit tests
